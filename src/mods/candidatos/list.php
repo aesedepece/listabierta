5782715aa018154f85231fd0547415e6c2a6056c
Adán Sánchez de Pedro Crespo <pedrocrespo@kadede.com>
2012-01-10 01:29:26 +0100

    [candidatos] Nuevos submódulos 'candidature', 'list' y 'region'

---
+++ src/mods/candidatos/list.php
@@ -0,0 +1,15 @@
+<?php
+$regions = mysql_query("SELECT * FROM `regions` WHERE `enabled` = '1' ORDER BY `name` ASC", $db);
+?>
+<section id="main">
+	<header>
+		<h1>CANDIDATOS</h1>
+	</header>
+	<section id="regions">
+		<?php while($region = mysql_fetch_assoc($regions))echo"<article class=\"region\"><header><a href=\"candidatos/".strtolower(utf8_encode($region['name']))."\">".ucfirst(utf8_encode($region['name']))."</a></header></article>"; ?>	
+	</section>
+	<aside id="joinus">
+		<a href="candidatos/presentarme"><img src="<?php echo$r; ?>mods/candidatos/img/banner240x275.png" alt="Buscamos candidatos como TÚ. Apúntate >" /></a>
+	</aside>
+	<footer></footer>
+</section>
